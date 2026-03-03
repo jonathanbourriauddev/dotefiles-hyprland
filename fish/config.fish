06a80e5ecc26c4f7a1e7a51422e43fd039cca40e
jonathanbourriauddev <jonathan.bourriauddev@gmail.com>
2026-03-03 20:44:05 +0100

    Force Fish shell dans la config Kitty

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    set fish_greeting
+    alias ff="fastfetch"
+    alias gpsh="cd ~/dotefiles-hyprland && git add . && git commit -m 'Update' && git push"
+    alias v="nvim"
+    alias h="hx"
+end
